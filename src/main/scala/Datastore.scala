b68b3cde59149a162bd2fe4b60444aac2458d4fc
Patrik Andersson <patrik.andersson@bynk.se>
2021-10-10 22:09:25 +0200

    Some error handling experiments.

--- src/main/scala/Datastore.scala
+++ src/main/scala/Datastore.scala
@@ -6,6 +6,7 @@ import doobie.postgres._
 import doobie.postgres.implicits._
 
 import cats._
+import cats.implicits._
 import cats.effect._
 
 import java.util.UUID
@@ -25,16 +26,41 @@ trait External[A] {
   def fromExternalRepresentation(e: ExternalRepresentation): Option[A]
 }
 
+object External {
+  def apply[E : External] = implicitly[External[E]]
+}
+
 trait EventstoreAlgebra[F[_]] {
   // Would it be relevant to expose some Doobie-bits here 
   // to make it possible to join transactions?
   def append[E : External](e : E): F[Unit]
+
   def stream[E : External](filter : Filter.Query): F[List[E]]
 }
 
 
 object Eventstore {
-  def apply[F[_] : Monad] = new EventstoreAlgebra[F] {
+  def apply[F[_]: Sync](xa: Transactor[F]) = new EventstoreAlgebra[F] {
 
+    // I might just aswell return the ConnectionIO thing outside.
+    def append[E: External](e: E): F[Unit] = {
+      val rep = External[E].externalRepresentation(e)
+      sql"".update.run.map(_ => ()).transact(xa)
+    }
+
+    def stream[E: External](filter: Filter.Query): F[List[E]] = {
+      // This fails if External does not recognize `kind` as an 
+      // Event. Use raiseError here. Don't use exception.
+      def parse(e: ExternalRepresentation): F[E] =
+        External[E].fromExternalRepresentation(e) match {
+          case Some(x)   => ???
+          case otherwise => Sync[F].raiseError(???)
+        }
+
+        for {
+          xs <- sql"".query[ExternalRepresentation].to[List].transact(xa)
+          ys <- xs.traverse(parse)
+        } yield ys
+    }
   }
 }
